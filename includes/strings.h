5939fa7b773b1b5e03e2f57e130913c1e1808b9b
1M0reBug <jordan.quagliatini@gmail.com>
2016-02-18 17:11:44 +0100

    Essaie avec les chaines de caracteres

---
+++ includes/strings.h
@@ -0,0 +1,16 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+#include <string.h>
+#include "types.h"
+
+void str_afficher(strings* l);
+strings* str_ajoute_fin(strings* l, char* chaine);
+strings* str_ajoute_debut(strings* l, char* chaine);
+strings* str_ajoute_position(strings* l, char* chaine, unsigned int position);
+strings* str_remplir(strings* l, char* tab[], unsigned int nb);
+int str_rechercher(strings* l, char* chaine);
+strings* str_supprimer(strings* l, char* chaine);
+void str_vider(strings** l);
+
+#endif //STRINGS_H
--- includes/types.h
+++ includes/types.h
@@ -2,10 +2,16 @@
 #define TYPES_H
 
 typedef struct _liste liste;
+typedef struct _strings strings;
 
 struct _liste {
     int valeur;
     liste *suivant;
+};
+
+struct _strings {
+    char* valeur;
+    strings *suivant;
 };
 
 typedef enum {false, true} bool;
