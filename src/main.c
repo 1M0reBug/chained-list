b9c04b9f904c8c1a8b13a343e53049243cbd0a25
1M0reBug <jordan.quagliatini@gmail.com>
2016-02-18 16:32:12 +0100

    gestion exception, nettoyage du code

--- src/main.c
+++ src/main.c
@@ -13,29 +13,40 @@ int main(void) {
   liste* ma_liste = NULL;
   int tab[10] = {1,2,3,4,5,6,7,8,9,10};
 
-  ma_liste = remplir(ma_liste,tab,10);
+  printf("Remplir la liste :\n");
+  ma_liste = remplir(ma_liste, tab, 10);
   afficher(ma_liste);
+  printf("---\n");
 
+  printf("\nSupprimer le 4 :\n");
   supprimer(ma_liste, 4);
-
-  printf("---\n");
   afficher(ma_liste);
+  printf("---\n");
 
+  printf("\nSuppression du premier:\n");
+  ma_liste = supprimer(ma_liste, 1);
+  afficher(ma_liste);
+  printf("---\n");
+
+  printf("\nAjoute position 4, 4:\n");
   ajoute_position(ma_liste, 4, 4);
-
-  printf("---\n");
   afficher(ma_liste);
+  printf("---\n");
 
+
+  printf("\nAjoute Debut 0:\n");
   ajoute_debut(ma_liste, 0);
-
-  printf("---\n");
   afficher(ma_liste);
-
-
   printf("---\n");
-  printf("la valeur 10 a ete trouvee a la position : %d\n", rechercher(ma_liste, 10));
 
+
+  printf("\nRecherche de la valeur 10:\n");
+  printf("la valeur 10 a ete trouvee a la position : %d\n", rechercher(ma_liste, 10));
+  printf("---\n");
+
+  printf("\nVidange de la liste:\n");
   vider(&ma_liste);
+  printf("---\n");
 
 
   return 0;
