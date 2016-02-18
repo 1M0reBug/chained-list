0f74739690db4ffcc5066204baf59ee7d0fa5761
1M0reBug <jordan.quagliatini@gmail.com>
2016-02-18 15:42:03 +0100

    tests: :white_check_mark: v1.0

--- src/main.c
+++ src/main.c
@@ -13,8 +13,28 @@ int main(void) {
   liste* ma_liste = NULL;
   int tab[10] = {1,2,3,4,5,6,7,8,9,10};
 
-  remplir(ma_liste,tab,10);
+  ma_liste = remplir(ma_liste,tab,10);
   afficher(ma_liste);
+
+  supprimer(ma_liste, 4);
+
+  printf("---\n");
+  afficher(ma_liste);
+
+  ajoute_position(ma_liste, 4, 4);
+
+  printf("---\n");
+  afficher(ma_liste);
+
+  ajoute_debut(ma_liste, 0);
+
+  printf("---\n");
+  afficher(ma_liste);
+
+
+  printf("---\n");
+  printf("la valeur 10 a ete trouvee a la position : %d\n", rechercher(ma_liste, 10));
+
   vider(&ma_liste);
 
 
